9a76dc0e643aa7bc56b257361b7513ab00fc2c72
JonM <jonmisner86@gmail.com>
2020-10-23 18:07:44 -0500

    front end and server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const path = require("path");
+
+const app = espress();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+   console.log("App running on port 3000!");
+ });

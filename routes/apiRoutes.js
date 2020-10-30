dcca84bbe343f3ab6a1f33f4887d13263a4bedb4
JonM <jonmisner86@gmail.com>
2020-10-29 23:43:05 -0500

    api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -23,11 +23,25 @@ router.get("/api/workouts", (req,res) => {
 });
 
 router.put("/api/workouts/:id", ({body, params}, res) =>{
-
+   db.findByIdAndUpdate( body.params.id, {$push: {exercises: body.body}},
+     (error, dbWorkout) => {
+        if (error) {
+           res.send(error);
+        } else{
+           res.send(dbWorkout)
+        }
+     }
+   );
 });
 
 router.get("/api/workouts/range", (req,res) => {
-
+   db.find({})
+   .then(dbWorkout => {
+      res.json(dbWorkout);
+   })
+   .catch(err => {
+      res.status(400).json(err);
+   });
 });
 
 router.delete("/api/workouts", ({body}, res) => {
